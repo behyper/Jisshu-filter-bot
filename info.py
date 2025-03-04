8182ce47e3e8e390914a78b3adb09b42465dd76e
Hyper <145529358+behyper@users.noreply.github.com>
2025-03-04 16:14:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://experienced-noellyn-yok-a8a5148a.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
